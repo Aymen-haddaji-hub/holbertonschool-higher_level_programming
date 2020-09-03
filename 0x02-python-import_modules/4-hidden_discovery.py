8842942b2910d1b16f81a949adbe75582faee38c
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-09-03 17:42:52 +0100

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-    a = dir(hidden_4)
-    for i in a:
-        if i[:2] != "__":
-            print(i)
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
