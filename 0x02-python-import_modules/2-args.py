aab8d7fe8443d8632654234b6542413ae67fbc53
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-09-03 15:16:27 +0100

    args as a list

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,3 +4,7 @@ if __name__ == "__main__":
     l = len(argv)
     print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
                                  "." if l == 1 else ":"))
+    for index, alist in enumerate(args):
+        if index > 0:
+            print("{:d} {:s}".format(index, alist))
+
