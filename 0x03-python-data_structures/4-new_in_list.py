a2a3e9892f457fa8ddf341561338bd5a1e294b4a
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-09-08 15:04:53 +0100

    check code errors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    a = len(my_list)
     edited_list = my_list[:]
-    if idx < 0 or idx > (a +1):
+    if idx < 0 or idx > len(my_list):
         return edited_list
     edited_list[idx] = element
     return edited_list
