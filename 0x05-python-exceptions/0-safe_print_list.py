1579017f2bfae58c6c36be4753673c87a382975e
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-09-21 14:10:09 +0100

    try_excep

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/user/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    while counter < x:
+    for i in range (x):
         try:
-            print(my_list[counter], end="")
+            print(my_list[i], end="")
             counter += 1
         except IndexError:
             break
