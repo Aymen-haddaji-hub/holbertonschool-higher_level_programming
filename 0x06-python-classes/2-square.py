fd4484779a2b0bcc5e09a390f8fba9aac87e3c15
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-09-22 16:21:38 +0100

    fix docstr

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,17 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """
-This is the Square module.
 """
 
+
 class Square:
-	"""our class"""
+    """our class
+    """
     def __init__(self, size=0):
-    	"""the init method"""
-        if type(size) != int:
+        """__init__ method
+        """
+        if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+
         self.__size = size
