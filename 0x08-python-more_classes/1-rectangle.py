1cb8886679a699721640d182bbd982b48a05ae07
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-09-28 12:27:27 +0100

    adding empty files

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Rectangle class
+"""
+
+class Rectangle:
+	""" Rectangle class"""
+	def __init__(self, width=0, height=0):
+		"""instantiation with widht and hight"""
+		self.width = width
+		self.height = height
+
+	@property
+	def width(self):
+		"""__width getter"""
+		return self.__width
+	@width.setter
+	def width(self, value):
+		"""__width setter"""
+		if type(value) != int:
+			raise TypeError("width must be an integer")
+		if value < 0:
+			raise ValueError("width must be >= 0")
+		self.__width = value
+
+	@property
+	def height(self):
+		""" height getter"""
+		return self.__height
+	@height.setter
+	def height(self, value):
+		""" height setter"""
+		if type(value) != 0:
+			raise TypeError("height must be an integer")
+		if value < 0:
+			raise ValueError("height must be >= 0")
+		self.__height = value
