dc55c9690422863f12b950b48f49270be2b4a4b9
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-09-29 14:20:25 +0100

    pushing to check docstring

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+square class
+"""
 class Square:
     """"class"""
     def __init__(self, size=0):
@@ -11,11 +14,13 @@ class Square:
         return self.__size
     @size.setter
     def size(self, value):
+        """ the size of square"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
+        """area of square"""
         size = self.__size
         return(size ** 2)
