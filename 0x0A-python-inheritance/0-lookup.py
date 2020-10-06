31a6b5f7ba9ec731489691cfea8b50162f9527eb
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-10-06 16:03:36 +0100

    list of attributes abd methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" avialable attributes and methods of an object """
+
+
+def lookup(obj):
+    """
+    Returns the list of avialable attributes and methods of an object
+    Args:
+    obj (object): the object to look up.
+
+    Return (List): list of objects.
+"""
+    return dir(obj)
