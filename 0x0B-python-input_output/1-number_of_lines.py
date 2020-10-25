14b6a80c66798c2ba45216b09af1f845339c1bb3
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-10-25 12:40:57 +0100

    count number of lines in a .txt file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Count number of Lines """
+
+
+def number_of_lines(filename=""):
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
+    return lines
