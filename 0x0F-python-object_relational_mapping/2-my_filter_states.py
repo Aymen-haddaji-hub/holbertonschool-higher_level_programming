8f98dfb297f6f19b92b2b19132a63bd4168653e0
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-13 17:49:14 +0100

    search by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-""" Python x MySQL : Listing data from a database using case sensitive """
+""" 
+Search data in db
+"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
+                         port=3306,
                          user=argv[1],
                          passwd=argv[2],
-                         db=argv[3],
-                         charset="utf8mb4")
+                         db=argv[3])
     c = db.cursor()
-    c.execute("SELECT * from states WHERE name LIKE '{}'\
-            COLLATE latin1_general_cs\
-            ORDER BY states.id".format(argv[4]))
+    c.execute("SELECT * from states WHERE name LIKE '{}' COLLATE latin1_general_cs\
+                ORDER BY states.id".format(argv[4]))
     for state in c.fetchall():
         print(state)
     c.close()
