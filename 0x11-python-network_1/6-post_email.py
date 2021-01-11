5537252f9af16f85365f162417dc02757669e2ae
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-01-11 13:43:52 +0100

    correct output to text format

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,12 +9,7 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    try:
-        url = argv[1]
-        mail_adress = {"email": argv[2]}
-        data = requests.post(url, data=mail_adress)
-        print(data.text)
-    except Exception:
-        if not argv[1] or argv[2]:
-            pass
-        
+    url = argv[1]
+    mail_adress = {"email": argv[2]}
+    data = requests.post(url, data=mail_adress)
+    print(data.text)
