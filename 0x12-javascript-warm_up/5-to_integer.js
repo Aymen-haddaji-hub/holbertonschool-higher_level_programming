fb39a5d10cf0e8043ea7f2272049d5984ef32897
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-05 19:39:41 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2]))
-{
+if (parseInt(process.argv[2])) {
   console.log('My number: ' + process.argv[2]);
-}
-else
-{
+} else {
   console.log('Not a number');
 }
