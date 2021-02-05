ab65ca1b4d1467c63f8a741acc7866f9ba05c385
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-05 21:27:03 +0100

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (n < 2 && n > 0) { return (1); } else if (n < 0) { return (-1); } else if (n > 1) { return (n * factorial(n - 1)); } else { return (0); }
+  if (n < 2 && n > 0) { return (1); } else if (n > 1) { return (n * factorial(n - 1)); } else { return (0); }
 }
 console.log(factorial(parseInt(process.argv[2])));
