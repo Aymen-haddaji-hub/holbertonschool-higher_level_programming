27a28164885bc2cf9146379b94fb32b936978907
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-09 20:16:23 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const adress = 'http://swapi.co/api/films/' + process.argv[2];
-request(adress, (error, body) => {
-        console.log(error || JSON.parse(body).title);
-    });
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
